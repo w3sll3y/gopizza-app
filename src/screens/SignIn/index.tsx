2aa63fcf7710068e334fc08d813904a3ffa7de69
Wesley Fernandes <fernandess.weslley@gmail.com>
2022-07-19 15:22:50 -0300

    finalizing login layout

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,28 +1,47 @@
 import React from "react";
+import { KeyboardAvoidingView, Platform } from "react-native";
+import brandImg from '../../assets/brand.png';
+
 import { Input } from "../../components/Input";
-import { Container } from "./styles";
+import {
+  Container,
+  Content,
+  Title,
+  Brand,
+  ForgotPasswordButton,
+  ForgotPasswordLabel
+} from "./styles";
+
 import { Button } from "../../../src/components/Button";
+
 export function SignIn() {
   return (
     <Container>
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <Content>
+          <Brand source={brandImg} />
+          <Title> Login </Title>
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
 
-      <Input
-        placeholder="E-mail"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitaliza="none"
-      />
-
-      <Input
-        placeholder="Senha"
-        type="secondary"
-        secureTextEntry
-      />
-
-      <Button
-        title="Entrar"
-        type="secondary"
-      />
+          <Input
+            placeholder="Senha"
+            type="secondary"
+            secureTextEntry
+          />
+          <ForgotPasswordButton>
+            <ForgotPasswordLabel> Esqueci minha senha </ForgotPasswordLabel>
+          </ForgotPasswordButton>
+          <Button
+            title="Entrar"
+            type="secondary"
+          />
+        </Content>
+      </KeyboardAvoidingView>
     </Container >
   );
 }

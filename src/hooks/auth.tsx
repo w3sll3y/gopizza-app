165cec3ce5c4631f1b92da9ad7520214d38b6cb3
Wesley Fernandes <fernandess.weslley@gmail.com>
2022-07-20 18:56:11 -0300

    Auth

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -6,10 +6,18 @@ import React, {
 } from "react";
 import { Alert } from "react-native";
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+
+type User = {
+  id: string,
+  name: string,
+  isAdmin: boolean
+}
 
 type AuthContextData = {
   signIn: (email: string, password: string) => Promise<void>;
-  isLogging: boolean
+  isLogging: boolean;
+  user: User | null;
 }
 
 type AuthProviderProps = {
@@ -19,8 +27,8 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-
   const [isLogging, setIsLogging] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
 
   async function signIn(email: string, password: string) {
     if (!email || !password) {
@@ -31,7 +39,24 @@ function AuthProvider({ children }: AuthProviderProps) {
 
     auth().signInWithEmailAndPassword(email, password)
       .then(account => {
-        console.log(account);
+        firestore()
+          .collection('users')
+          .doc(account.user.uid)
+          .get()
+          .then(profile => {
+            const { name, isAdmin } = profile.data() as User;
+
+            if (profile.exists) {
+              const userData = {
+                id: account.user.uid,
+                name,
+                isAdmin
+              };
+
+              console.log(userData);
+              setUser(userData)
+            }
+          })
       })
       .catch(error => {
         const { code } = error;
